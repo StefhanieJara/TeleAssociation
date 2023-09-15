a463bce9463d3bdc11c43608c5c6256e1c5efbc2
Miguel <a20190050@pucp.edu.pe>
2023-09-15 17:28:44 -0500

    q hvadaa

---
+++ app/src/main/java/com/example/teleassociation/donacionesAdmin.java
@@ -0,0 +1,14 @@
+package com.example.teleassociation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class donacionesAdmin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donaciones_admin);
+    }
+}
