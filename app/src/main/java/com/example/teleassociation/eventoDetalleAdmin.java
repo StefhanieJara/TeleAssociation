40b60419cd3993483f0441adee7d8f5dfacb15f6
Miguel <a20190050@pucp.edu.pe>
2023-09-15 14:11:55 -0500

    q hvada

---
+++ app/src/main/java/com/example/teleassociation/eventoDetalleAdmin.java
@@ -0,0 +1,14 @@
+package com.example.teleassociation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class eventoDetalleAdmin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_evento_detalle_admin);
+    }
+}
