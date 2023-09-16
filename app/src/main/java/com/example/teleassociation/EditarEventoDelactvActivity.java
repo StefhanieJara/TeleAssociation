d3371c5cbd9933b286af6e4740719d5b90757529
leonardo <a20201696@pucp.edu.pe>
2023-09-16 07:15:44 -0500

    lo di todo

---
+++ app/src/main/java/com/example/teleassociation/EditarEventoDelactvActivity.java
@@ -0,0 +1,14 @@
+package com.example.teleassociation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditarEventoDelactvActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editar_evento_delactv);
+    }
+}
---
+++ app/src/main/java/com/example/teleassociation/MasEventosDelactvActivity.java
@@ -0,0 +1,14 @@
+package com.example.teleassociation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MasEventosDelactvActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mas_eventos_delactv);
+    }
+}
