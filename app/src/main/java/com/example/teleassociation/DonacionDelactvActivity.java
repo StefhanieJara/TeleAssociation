4e83dac3dbf70d0f5723ac41981993570de46f56
leonardo <a20201696@pucp.edu.pe>
2023-09-16 03:48:52 -0500

    avance uwu

---
+++ app/src/main/java/com/example/teleassociation/DonacionDelactvActivity.java
@@ -0,0 +1,13 @@
+package com.example.teleassociation;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DonacionDelactvActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donacion_delactv);
+    }
+}
