a3a9f2a4823685d66b0f65dc15bda00cc6906cd9
Stefhanie Jaramillo <a20190674@pucp.edu.pe>
2023-09-23 01:30:05 -0500

    ultimos cambios

---
+++ app/src/main/java/com/example/teleassociation/ListaDeEventosReal.java
@@ -0,0 +1,14 @@
+package com.example.teleassociation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListaDeEventosReal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_de_eventos_real);
+    }
+}
