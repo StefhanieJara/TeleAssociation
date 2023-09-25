006dcc4bb550f34a91e1316f26ff75dc3e8453ec
Miguel <a20190050@pucp.edu.pe>
2023-09-24 22:28:46 -0500

    avance vistas admin, me quede en validar participante ga

---
+++ app/src/main/java/com/example/teleassociation/validarParticipanteAdmin.java
@@ -0,0 +1,13 @@
+package com.example.teleassociation;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class validarParticipanteAdmin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_validar_participante_admin);
+    }
+}
