76b99aad4775e4815118ee4a69e01a91b44435ef
leonardo <a20201696@pucp.edu.pe>
2023-09-26 03:00:34 -0500

    Mini avance

---
+++ app/src/main/java/com/example/teleassociation/CrearActividadDelactvActivity.java
@@ -0,0 +1,14 @@
+package com.example.teleassociation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CrearActividadDelactvActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_actividad_delactv);
+    }
+}
---
+++ app/src/main/java/com/example/teleassociation/ListaActividadesDelactvActivity.java
@@ -0,0 +1,14 @@
+package com.example.teleassociation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListaActividadesDelactvActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_actividades_delactv);
+    }
+}
