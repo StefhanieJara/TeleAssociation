961eccd9431dcb6f369b18364b0585edc6d69125
Stefhanie Jaramillo <a20190674@pucp.edu.pe>
2023-09-29 01:02:13 -0500

    cambios

---
+++ app/src/main/java/com/example/teleassociation/inicioAdminActividad.java
@@ -0,0 +1,14 @@
+package com.example.teleassociation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class inicioAdminActividad extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio_admin_actividad);
+    }
+}
--- app/src/main/java/com/example/teleassociation/inicio_usuario.java
+++ app/src/main/java/com/example/teleassociation/inicio_usuario.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ListaDeEventosReal extends AppCompatActivity {
+public class inicio_usuario extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_lista_de_eventos_real);
+        setContentView(R.layout.activity_inicio_usuario);
     }
 }
