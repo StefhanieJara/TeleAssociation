71c5e9b92d5cdbfa9e11875ecf6bb4b74f61079a
DiegoLG15 <a20200839@pucp.edu.pe>
2023-09-29 02:48:26 -0500

    coexion de vistas + text view

--- app/src/main/java/com/example/teleassociation/MainActivity.java
+++ app/src/main/java/com/example/teleassociation/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -52,6 +53,10 @@ public class MainActivity extends AppCompatActivity {
 
         });
 
+    }
+    public void registrarse(View view){
+        Intent intent=new Intent(this, Registrarse.class);
+        startActivity(intent);
     }
 
 }
--- app/src/main/java/com/example/teleassociation/inicio_usuario.java
+++ app/src/main/java/com/example/teleassociation/inicio_usuario.java
@@ -2,7 +2,9 @@ package com.example.teleassociation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class inicio_usuario extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class inicio_usuario extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio_usuario);
+    }
+    public void verMasEventos(View view){
+        Intent intent=new Intent(this, misEventosUsuario.class);
+        startActivity(intent);
     }
 }
