6f3acc0692b786686f6133d25464cc43d4981907
leonardo <a20201696@pucp.edu.pe>
2023-09-30 11:48:07 -0500

    ultimo commit espero

--- app/src/main/java/com/example/teleassociation/ListaEventosActivity.java
+++ app/src/main/java/com/example/teleassociation/ListaEventosActivity.java
@@ -35,7 +35,7 @@ public class ListaEventosActivity extends AppCompatActivity {
         boton4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(ListaEventosActivity.this, MasEventosDelactvActivity.class);
+                Intent intent = new Intent(ListaEventosActivity.this, MisEventosDelactvActivity.class);
                 startActivity(intent);
             }
         });
