44375a37bb3ac54383bccb7e9762a4e533c50f47
DiegoLG15 <a20200839@pucp.edu.pe>
2023-10-17 22:00:46 -0500

    upload image

--- app/src/main/java/com/example/teleassociation/MainActivity.java
+++ app/src/main/java/com/example/teleassociation/MainActivity.java
@@ -1,8 +1,18 @@
 package com.example.teleassociation;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static android.Manifest.permission.POST_NOTIFICATIONS;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -17,12 +27,14 @@ import java.util.Map;
 public class MainActivity extends AppCompatActivity {
     HashMap<String,String> credencial= new HashMap<>();
     ActivityMainBinding binding;
-
+    String channelId = "channelDefaultPri";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding=ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        createNotificationChannel();
 
         credencial.put("w","12345");
         credencial.put("leo.abanto@gmail.com","12345");
@@ -64,5 +76,44 @@ public class MainActivity extends AppCompatActivity {
         Intent intent=new Intent(this, Registrarse.class);
         startActivity(intent);
     }
+    public void createNotificationChannel() {
+        //android.os.Build.VERSION_CODES.O == 26
+        NotificationChannel channel = new NotificationChannel(channelId,
+                "Canal notificaciones default",
+                NotificationManager.IMPORTANCE_DEFAULT);
+        channel.setDescription("Canal para notificaciones con prioridad default");
+
+        NotificationManager notificationManager = getSystemService(NotificationManager.class);
+        notificationManager.createNotificationChannel(channel);
+
+        askPermission();
+
+    }
+    public void askPermission(){
+        //android.os.Build.VERSION_CODES.TIRAMISU == 33
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU && ActivityCompat.checkSelfPermission(this, POST_NOTIFICATIONS) == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{POST_NOTIFICATIONS},101);
+        }
+    }
+    public void notificarImportanceDefault(){
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, channelId)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle("Mi primera notificación")
+                .setContentText("Esta es mi primera notificación en Android :D")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+        if (ActivityCompat.checkSelfPermission(this, POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED) {
+            notificationManager.notify(1, builder.build());
+        }
+    }
+
+
 
 }
