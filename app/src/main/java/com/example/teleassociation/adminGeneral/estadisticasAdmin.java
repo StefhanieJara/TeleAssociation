df3c24f6ea6ab4f91901d8e5302d2c37c043bead
Stefhanie Jaramillo <a20190674@pucp.edu.pe>
2023-11-04 01:50:21 -0500

    estadisticas no se la bajen plis

--- app/src/main/java/com/example/teleassociation/adminGeneral/estadisticasAdmin.java
+++ app/src/main/java/com/example/teleassociation/adminGeneral/estadisticasAdmin.java
@@ -1,13 +1,27 @@
 package com.example.teleassociation.adminGeneral;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.example.teleassociation.MainActivity;
 import com.example.teleassociation.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class estadisticasAdmin extends AppCompatActivity {
+
+    AdminGeneralInicioFragment adminGeneralInicioFragment = new AdminGeneralInicioFragment();
+    ListaActividadesGeneralFragment listaActividadesGeneralFragment = new ListaActividadesGeneralFragment();
+    CrearActividadFragment crearActividadFragment = new CrearActividadFragment();
+
+    PersonasGeneralFragment personasGeneralFragment = new PersonasGeneralFragment();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +32,47 @@ public class estadisticasAdmin extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.hide();
         }
+
+        BottomNavigationView navigation = findViewById(R.id.bottom_navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+    }
+
+    private final BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            if(item.getItemId()==R.id.firstFragment){
+                loadFragment(adminGeneralInicioFragment);
+                return true;
+            }
+            if(item.getItemId()==R.id.secondFragment){
+                loadFragment(listaActividadesGeneralFragment);
+                return true;
+            }
+            if(item.getItemId()==R.id.thirdFragment){
+                loadFragment(crearActividadFragment);
+                return true;
+            }
+            if(item.getItemId()==R.id.fourFragment){
+                loadFragment(personasGeneralFragment);
+                return true;
+            }
+            if(item.getItemId()==R.id.fiveFragment){
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                return true;
+            }
+            return false;
+        }
+    };
+
+
+
+
+    public void loadFragment(Fragment fragment){
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_container,fragment);
+        transaction.commit();
+
     }
 }
