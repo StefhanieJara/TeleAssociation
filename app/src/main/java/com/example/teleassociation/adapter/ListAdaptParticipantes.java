3963e6b163b95bdf1b93e5e0cba9e61b1e8659fc
Miguel <a20190050@pucp.edu.pe>
2023-12-02 23:25:41 -0500

    avanzando mas

--- app/src/main/java/com/example/teleassociation/adapter/ListAdaptParticipantes.java
+++ app/src/main/java/com/example/teleassociation/adapter/ListAdaptParticipantes.java
@@ -2,6 +2,7 @@ package com.example.teleassociation.adapter;
 
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,12 +10,15 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.teleassociation.ListaParticipantes;
 import com.example.teleassociation.R;
 import com.example.teleassociation.dto.eventoListarUsuario;
 import com.example.teleassociation.dto.participante;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.List;
 
@@ -22,6 +26,8 @@ public class ListAdaptParticipantes extends RecyclerView.Adapter<ListAdaptPartic
     private List<participante> participanteList;
     private Context context;
     private MisEventAdapterAdminActv.OnVerEventoClickListener listener;
+    FirebaseFirestore db;
+    String asignacion;
 
 
     public interface OnVerEventoClickListener {
@@ -64,7 +70,16 @@ public class ListAdaptParticipantes extends RecyclerView.Adapter<ListAdaptPartic
         holder.codigo.setText(part.getCodigo());
         holder.asignacion.setText(part.getAsignacion());
 
-        // Aquí puedes asignar otros datos como la descripción, estado, actividad, etc.
+        if(part.getAsignacion().equals("Delegado")){
+            holder.verEvento.setVisibility(View.GONE);
+        }else{
+            // Aquí puedes asignar otros datos como la descripción, estado, actividad, etc.
+            holder.verEvento.setOnClickListener(v -> {
+                // Implementar lógica para abrir el popup y actualizar Firebase
+                mostrarPopupYActualizarFirebase(part.getEvento(), part.getCodigo(), holder.asignacion);
+            });
+        }
+
 
     }
 
@@ -86,5 +101,54 @@ public class ListAdaptParticipantes extends RecyclerView.Adapter<ListAdaptPartic
             asignacion = itemView.findViewById(R.id.asignacion);
             verEvento = itemView.findViewById(R.id.verEvento);
         }
+    }
+
+    private void mostrarPopupYActualizarFirebase(String evento, String codigo, TextView asignacionTextView) {
+        // Implementa el código para mostrar un popup y permitir al usuario elegir 'barra' o 'jugador'
+        // Puedes usar un AlertDialog con opciones o implementar un diálogo personalizado.
+
+        // Ejemplo usando AlertDialog con lista de opciones
+        String[] opciones = {"barra", "jugador"};
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle("Elige una opción");
+        builder.setItems(opciones, (dialog, which) -> {
+            asignacion = opciones[which]; // Obtiene la opción seleccionada ('Barra' o 'Jugador')
+
+            Log.d("msg-test", "El usuario a cambiar es " + codigo + " del evento: "+ evento + " y con nueva asignacion: "+asignacion);
+            // Actualiza Firebase con la nueva asignación
+            actualizarFirebase(evento, codigo, asignacion, asignacionTextView);
+        });
+
+        builder.show();
+    }
+
+    private void actualizarFirebase(String evento, String codigo, String nuevaAsignacion, TextView asignacionTextView) {
+        // Accede a la colección 'participantes' y actualiza el documento correspondiente
+        db = FirebaseFirestore.getInstance();
+        db.collection("participantes")
+                .whereEqualTo("evento", evento)
+                .whereEqualTo("codigo", codigo)
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful() && !task.getResult().isEmpty()) {
+                        // Actualiza el campo 'asignacion' con la nueva asignación
+                        DocumentSnapshot participanteDocument = task.getResult().getDocuments().get(0);
+                        participanteDocument.getReference().update("asignacion", nuevaAsignacion)
+                                .addOnSuccessListener(aVoid -> {
+                                    // Éxito al actualizar Firebase
+                                    Log.d("msg-test", "Asignación actualizada con éxito a: " + nuevaAsignacion);
+
+                                    // Actualiza el TextView con la nueva asignación
+                                    asignacionTextView.setText(nuevaAsignacion);
+                                })
+                                .addOnFailureListener(e -> {
+                                    // Error al actualizar Firebase
+                                    Log.e("msg-test", "Error al actualizar asignación", e);
+                                });
+                    } else {
+                        Log.e("msg-test", "Error al buscar el participante en Firebase", task.getException());
+                    }
+                });
     }
 }
